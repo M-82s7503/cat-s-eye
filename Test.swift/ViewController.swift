cac8879325143b2cb1f911c775f09ba1a36ab47a
Fukuda Kouki <e165727@ie.u-ryukyu.ac.jp>
2017-11-21 15:07:41 +0900

    first commit

---
+++ Test.swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test.swift
+//
+//  Created by 福田　光希 on 2017/11/21.
+//  Copyright © 2017年 福田　光希. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
