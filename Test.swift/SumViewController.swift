5deed606af43b532762dcca211560fbf0b490ba9
Motoharu Kumanomido <e165720@.u-ryukyu.ac.jp>
2017-11-28 15:58:14 +0900

    UIをいじった． ViewControllerを新しく一つ追加して， それと関連付けするsumViewControllerClass.swiftを追加． label, buttonとスクリプトと関連させた．

---
+++ Test.swift/SumViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SumViewController.swift
+//  Test.swift
+//
+//  Created by 熊埜御堂元晴 on 2017/11/28.
+//  Copyright © 2017年 福田　光希. All rights reserved.
+//
+
+import UIKit
+
+class SumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Test.swift/ViewController.swift
+++ Test.swift/ViewController.swift
@@ -20,7 +20,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+  /*以下の@がついた，４つの変数と，メソッドは画面のLabelとbuttonを関連付けしたもの．*/
+  @IBOutlet weak var numPeople: UILabel!
   
+  @IBOutlet weak var numGirls: UILabel!
+  
+  @IBOutlet weak var numHungry: UILabel!
+  
+  @IBOutlet weak var numNotHungry: UILabel!
+  
+  @IBAction func plusButton1(_ sender: AnyObject) {
+  }
+  
+  @IBAction func plusButton2(_ sender: AnyObject) {
+  }
+  
+  @IBAction func plusButton3(_ sender: AnyObject) {
+  }
+  
+  @IBAction func minusButton1(_ sender: AnyObject) {
+  }
+  
+  @IBAction func minusButton2(_ sender: AnyObject) {
+  }
+  
+  @IBAction func minusButton3(_ sender: AnyObject) {
+  }
   
 }
 
